1a344d416253e0ee41fd248a79101c7a642e73b9
wojtek11530 <wojtek19962a32@gmail.com>
2021-09-29 13:30:42 +0200

    feat: add script downloading Multiemo dataset

---
+++ scripts/download_dataset.py
@@ -0,0 +1,20 @@
+import os
+import requests
+import zipfile
+
+from settings import DATA_FOLDER
+
+url = 'https://clarin-pl.eu/dspace/bitstream/handle/11321/798/multiemo.zip?sequence=2&isAllowed=y'
+
+output_zip = os.path.join(DATA_FOLDER, 'multiemo.zip')
+
+response = requests.get(url, stream=True)
+if response.status_code == 200:
+    with open(output_zip, 'wb') as f:
+        f.write(response.raw.read())
+
+with zipfile.ZipFile(output_zip, "r") as zip_ref:
+    zip_ref.extractall(DATA_FOLDER)
+
+os.remove(output_zip)
+os.remove(os.path.join(DATA_FOLDER, 'readme.txt'))
