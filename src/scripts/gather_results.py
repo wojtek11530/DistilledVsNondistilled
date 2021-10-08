a41258c7d32a06a02c8978817c8fdfe10e7fbc21
wojtek11530 <wojtek19962a32@gmail.com>
2021-10-08 14:56:47 +0200

    feat: add measuring parameters number and memory allocation

--- src/scripts/gather_results.py
+++ src/scripts/gather_results.py
@@ -4,7 +4,10 @@ import os
 from typing import Any, Dict
 
 import pandas as pd
+import torch
+from transformers import AutoModelForSequenceClassification
 
+from src.data.data_processing import get_num_labels
 from src.settings import DATA_FOLDER, MODELS_FOLDER
 
 
@@ -23,10 +26,10 @@ def main():
     models_folders = get_immediate_subdirectories(MODELS_FOLDER)
     data = []
     for model_folder in models_folders:
-        task_models_directories = get_immediate_subdirectories(model_folder)
-        for task_models_directory in task_models_directories:
-            if task_name in task_models_directory:
-                data_dict = gather_results(task_models_directory)
+        fine_tuned_model_directories = get_immediate_subdirectories(model_folder)
+        for ft_model_dir in fine_tuned_model_directories:
+            if task_name in ft_model_dir:
+                data_dict = gather_results(ft_model_dir, task_name)
                 data.append(data_dict)
 
     df = pd.DataFrame(data)
@@ -41,20 +44,39 @@ def get_immediate_subdirectories(directory: str):
             if os.path.isdir(os.path.join(directory, name))]
 
 
-def gather_results(task_models_directory: str) -> Dict[str, Any]:
-    with open(os.path.join(task_models_directory, 'training_params.json')) as json_file:
+def gather_results(ft_model_dir: str, task_name: str) -> Dict[str, Any]:
+    with open(os.path.join(ft_model_dir, 'training_params.json')) as json_file:
         training_data_dict = json.load(json_file)
 
-    with open(os.path.join(task_models_directory, 'test_results.json')) as json_file:
+    with open(os.path.join(ft_model_dir, 'test_results.json')) as json_file:
         test_data = json.load(json_file)
         [test_data_dict] = pd.json_normalize(test_data, sep='_').to_dict(orient='records')
 
     data = training_data_dict.copy()  # start with keys and values of x
     data.update(test_data_dict)
 
-    model_size = os.path.getsize(os.path.join(task_models_directory, 'pytorch_model.bin'))
+    model_size = os.path.getsize(os.path.join(ft_model_dir, 'pytorch_model.bin'))
     data['model_size'] = model_size
-    data['name'] = os.path.basename(task_models_directory)
+
+    num_labels = get_num_labels(task_name)
+
+    # LOADING THE BEST MODEL
+    model = AutoModelForSequenceClassification.from_pretrained(
+        ft_model_dir,
+        num_labels=num_labels
+    )
+
+    torch.cuda.empty_cache()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    data['memory_allocated'] = torch.cuda.memory_allocated(device)
+
+    parameters_num = 0
+    for n, p in model.named_parameters():
+        parameters_num += p.nelement()
+
+    data['parameters'] = parameters_num
+    data['name'] = os.path.basename(ft_model_dir)
     return data
 
 
