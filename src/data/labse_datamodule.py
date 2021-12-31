43b876e2f2d16225cd36342bc3cd804ccc5dd6a6
wojtek11530 <wojtek19962a32@gmail.com>
2021-12-31 11:57:15 +0100

    fixup! fixup! feat: correct LaBSE call

--- src/data/labse_datamodule.py
+++ src/data/labse_datamodule.py
@@ -85,8 +85,11 @@ class LabseDataset(Dataset):
             raise ValueError('Incorrect output mode')
 
         desc = f"Getting embeddings for {task_name}-{set_name}"
-        self.embedding_data = [np.array(embedder.encode(text, show_progress_bar=False))
-                               for text in tqdm(texts, desc=desc)]
+
+        # self.embedding_data = [np.array(embedder.encode(text, show_progress_bar=False))
+        #                        for text in tqdm(texts, desc=desc)]
+
+        self.embedding_data = embedder.encode(texts, show_progress_bar=True, convert_to_tensor=True)
         self.labels = all_label_ids
 
     def __getitem__(self, index: int) -> Tuple[np.ndarray, int]:
