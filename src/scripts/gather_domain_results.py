5bbde23dcb68d4349b4c0c7e0d7fa17c8ba777ed
wojtek11530 <wojtek19962a32@gmail.com>
2022-01-04 21:39:56 +0100

    fixup! fixup! feat: add script for gathering domain results

--- src/scripts/gather_domain_results.py
+++ src/scripts/gather_domain_results.py
@@ -12,6 +12,14 @@ from transformers import AutoModelForSequenceClassification
 from src.data.data_processing import get_num_labels
 from src.settings import DATA_FOLDER, MODELS_FOLDER_2
 
+models = [
+    'bert-base-uncased',
+    'distilbert-base-uncased',
+    'TinyBERT_General_4L_312D',
+    'TinyBERT_General_6L_768D',
+    'xtremedistil-l6-h256-uncased'
+]
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -29,13 +37,14 @@ def main():
         raise ValueError('task_level must be text or sentence')
 
     models_subdirectories = [x[0] for x in os.walk(MODELS_FOLDER_2)]
-    models_subdirectories = filter(lambda subdir: task_level in subdir and 'fasttext' not in subdir,
+    models_subdirectories = filter(lambda subdir: task_level in subdir and
+                                                  any([m in subdir for m in models]),
                                    models_subdirectories)
     models_subdirectories = sorted(models_subdirectories)
 
     data = list()
     for subdirectory in tqdm(models_subdirectories):
-        if task_level in subdirectory and 'fasttext' not in subdirectory:
+        if task_level in subdirectory and any([m in subdirectory for m in models]):
             data_dict_list = gather_results(subdirectory)
             for data_dict in data_dict_list:
                 data.append(data_dict)
